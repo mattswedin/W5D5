694b00cb7c504b97a07e7e253a9a78596c9c2f34
Matthew Swedin <matthew.swedin@gmail.comgit config --global user.email matthew.swedin@gmail.com>
2021-09-10 14:43:09 -0400

    01_queries complete

--- moviebuff/movie_buff/01_queries.rb
+++ moviebuff/movie_buff/01_queries.rb
@@ -66,10 +66,10 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-  # Movie
-  #   .select(:id, :title)
-  #   .joins(:director)
-  #   .where("actors.name" IN them)
+  Movie
+    .select(:id, :title)
+    .joins(:director)
+    .where("actors.name IN (?)", them)
 end
 
 def movie_names_before_1940
@@ -83,4 +83,6 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+  Movie.where('yr < 1940').pluck(:title)
+
 end
